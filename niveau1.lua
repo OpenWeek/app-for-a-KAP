d33b740191ddba638ee00ad15905b72929bf03e8
Tom Rousseaux <rousseaux1.linerider@gmail.com>
2018-07-05 14:21:25 +0200

    lvl1

--- niveau1.lua
+++ niveau1.lua
@@ -13,12 +13,42 @@ function level1()
 			launchMenuContinent(1,2)
 		end)
 	
-	local bu = Bitmap.new(Texture.new('button_up.png'))
-	local bd = Bitmap.new(Texture.new('button_down.png'))
-	local bt = TextField.new(nil, 'QUESTION BIATCH')
+	local bu1 = Bitmap.new(LongButtonUp)
+	local bd1 = Bitmap.new(LongButtonDown)
+	local b1 = Button.new(bu, bd, bt)
 	
-	local b = Button.new(bu, bd, bt)
+	local bu2 = Bitmap.new(LongButtonUp)
+	local bd2 = Bitmap.new(LongButtonDown)
+	local b2 = Button.new(bu, bd, bt)
 	
+	local bu3 = Bitmap.new(LongButtonUp)
+	local bd3 = Bitmap.new(LongButtonDown)
+	local b3 = Button.new(bu, bd, bt)
+	
+	function readFile(file, ist, other)
+		local istB = false
+		local otherB = false
+		for line in io.lines(file) do
+			if line == "<ist>" then
+				istB = true
+			elseif line == "</ist>" then
+				istB = false
+			else
+				if istB then
+					if line == "<other>" then
+						otherB = true
+					elseif line == "</other>" then
+						otherB = false
+					else
+						table.insert(other, line)
+					end
+				else
+					table.insert(ist, line)
+				end
+			end
+		end
+		
+	end
 	
 	
 	wallLevel1:addChild(back)
