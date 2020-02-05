fdd671e23d41aebaf77c33cf5371cac17d4a10cb
Magalii <magali.legast@student.uclouvain.be>
2020-02-05 22:25:26 +0100

    Better names in menus + better buttons position

--- kapotopia/core/src/gdx/kapotopia/Screens/MainMenu.java
+++ kapotopia/core/src/gdx/kapotopia/Screens/MainMenu.java
@@ -71,10 +71,10 @@ public class MainMenu implements Screen {
         final float x = game.viewport.getWorldWidth() / 2.6f;
         final float y = game.viewport.getWorldHeight();
         final TextButton world1 = new TextButtonBuilder(Localisation.getInstance().getString("text_world1"))
-                .withStyle(style).withPosition(x, y * 0.83f)
+                .withStyle(style).withPosition(x*0.7f, y * 0.75f)
                 .withListener(new ChangeScreenListener(game, ScreenType.WORLD1)).build();
         final TextButton world2 = new TextButtonBuilder(Localisation.getInstance().getString("text_world2"))
-                .withStyle(style).withPosition(x, y * 0.5f)
+                .withStyle(style).withPosition(x*0.82f, y * 0.43f)
                 .withListener(new ChangeScreenListener(game, ScreenType.WORLD2)).build();
         final TextButton world4 = new TextButtonBuilder(Localisation.getInstance().getString("text_istdex"))
                 .withStyle(style).withPosition(x, y * 0.2f)
