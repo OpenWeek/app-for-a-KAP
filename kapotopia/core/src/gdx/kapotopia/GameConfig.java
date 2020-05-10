9ef8113e7bd97957c85e8f3a98a0cd409e28438a
François Duchêne <duchene113@gmail.com>
2020-05-10 23:13:42 +0200

    Change to Alpha-0.4.5
    - Addition of skip buttons to skip game intros if the user already saw them
    - Addition of a small intro for the game
    - Musics in all screens instead of a few selected ones (only 4 screens had them)
    - Fixed bug in STIdex that unallowed the use of the back button, and removed the textual 'back' button

--- kapotopia/core/src/gdx/kapotopia/GameConfig.java
+++ kapotopia/core/src/gdx/kapotopia/GameConfig.java
@@ -5,8 +5,8 @@ import com.badlogic.gdx.utils.Logger;
 public class GameConfig {
 
     // TODO changer VERSION_NAME ET VERSION_CODE à chaque fois que l'on update le jeu, pas trouvé de moyen pour les liés automatiquement au gradle build d'android
-    public static final String VERSION_NAME = "Alpha-0.4.4";
-    public static final int VERSION_CODE = 17;
+    public static final String VERSION_NAME = "Alpha-0.4.5";
+    public static final int VERSION_CODE = 18;
 
     public static final float GAME_WIDTH = 720;
     public static final float GAME_HEIGHT = 1280;
